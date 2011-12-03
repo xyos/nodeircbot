48fd98fd6880f538b4ad6221cdc3cc5b52eb4427
Jairo Suarez <slayerjairo@gmail.com>
2011-12-03 22:09:00 +0000

    Adding basic functionality

--- irc.js
+++ irc.js
@@ -2,21 +2,49 @@ var net = require('net');
 var irc = {};
 
 irc.socket = new net.Socket();
+irc.socket.setEncoding('utf8');
+irc.socket.setNoDelay();
 
-irc.socket.on('data', funtion(data) {
-    data = data.split('\n');
-    data.forEach(logData);
-});
-
-function logData(element) {  
-    console.log("RECV -" + element);
-    if( element != "") {
-        irc.handle(element.slice(0,-1);
-    }
+irc.close = function(message){
+  message = typeof message != 'undefined' ? message : "Quit";
+  irc.raw('QUIT :' + message);
 }
 
-irc.socket.on('connect', function(){
-    irc.on(/^PING :(.+)$/i, function(info) {
-            irc.raw('PONG' + info[1]);
-    });
+irc.socket.on('data', function(data){
+  data = data.split("\n");
+  data.forEach(logData);
 });
+
+irc.raw = function(data){
+  if (irc.socket.write(data + '\n')){
+    console.log("SENT - " + data);
+  }
+}
+
+irc.listeners = [];
+irc.on = function(data, callback){
+  irc.listeners.push([data,callback,false]);
+}
+
+function logData(element){
+  if (element != ""){
+    console.log("RECV - " + element);
+  }
+}
+
+function ircConnect(){
+  console.log("connecting to " + irc.socket.remoteAddress);
+  irc.on(/^PING :(.+)$/i, function(info){
+    irc.raw('PONG :' + info[1]);
+  });
+  setTimeout(function(){
+    irc.raw('NICK nodebot');
+    irc.raw('USER nodebot * * :Jairo Suarez');
+    setTimeout(function(){
+      for (var i = 0; i < 5;i++){
+        irc.close();
+      }
+    }, 2000);
+  }, 1000);
+}
+irc.socket.connect(6667,'idsoftware.us.quakenet.org', ircConnect);
